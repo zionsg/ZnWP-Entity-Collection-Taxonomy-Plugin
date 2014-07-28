33ec0b96da5aa7da3684598b9ad634d8466ae960
Ng Kitt Horng, Zion <zion@intzone.com>
2014-07-28 20:37:21 +0800

    Update comments; Remove demo file include

--- znwp_entity_collection_taxonomy_plugin.php
+++ znwp_entity_collection_taxonomy_plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ZnWP Entity Collection Taxonomy Plugin
 Plugin URI:  https://github.com/zionsg/ZnWP-Entity-Collection-Taxonomy-Plugin
-Description: This plugin allows 3rd party plugins to easily add custom taxonomies for entities and collections and register with custom post types via an action hook. Collections can be color coded and entities can be linked to multiple collections. Default terms for each taxonomy can be added during activation and removed during deactivation of the 3rd party plugin. Demo code included at end of main plugin file.
+Description: This plugin allows multiple 3rd party plugins to easily add custom entity & collection taxonomies and register with custom post types via an action hook. Collections can be color coded and entities can be grouped under multiple collections. Default terms for each taxonomy are added during activation and removed during deactivation of the 3rd party plugin. Posts are linked to entities not collections - the latter is for visual grouping only. Demo plugin included.
 Author:      Zion Ng
 Author URI:  http://intzone.com/
 Version:     1.0.0
@@ -17,7 +17,3 @@ register_uninstall_hook(__FILE__,    array($znwp_entity_collection_taxonomy_plug
 
 // must be run after theme setup to allow functions.php in theme to add filter hook
 add_action('after_setup_theme', array($znwp_entity_collection_taxonomy_plugin, 'init'));
-
-
-/* DEMO CODE - Uncomment the following code to see the plugin in action */
-// include 'demo.php';
